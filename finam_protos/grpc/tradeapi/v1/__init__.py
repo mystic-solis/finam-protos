f2c35cab35e13b8c7a44384cafc436c6c4f76cbd
Влад Асламин <zero4one@ya.ru>
2025-12-15 23:31:13 +0500

    Добавил алиасы к модулям

--- finam_protos/grpc/tradeapi/v1/__init__.py
+++ finam_protos/grpc/tradeapi/v1/__init__.py
@@ -1,2 +1,9 @@
+from .accounts import accounts_service_pb2 as accounts_service
+from .assets import assets_service_pb2 as assets_service
+from .auth import auth_service_pb2 as auth_service
+from .marketdata import marketdata_service_pb2 as marketdata_service
+from .metrics import usage_metrics_service_pb2 as usage_metrics_service
+from .orders import orders_service_pb2 as orders_service
+
 from .side_pb2 import Side
 from .trade_pb2 import AccountTrade
